216c5c64d2f85dfe9614a22c6884c17ca10af669
Sarosij Bose <sarosijbose2000@gmail.com>
2022-02-11 22:21:43 +0530

    Create Net.py

---
+++ Net.py
@@ -0,0 +1,19 @@
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(3072, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc5 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
+        return F.log_softmax(x, dim=1)
+
+model  = Net()
+print(model)
